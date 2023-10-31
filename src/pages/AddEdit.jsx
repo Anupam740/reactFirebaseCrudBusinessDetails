cf26b1ed010d38cfe259f6e273f942e3983732ab
Anupam kumar <anupamkumar740@gmail.com>
2023-10-31 19:49:17 +0530

    second commit

--- src/pages/AddEdit.jsx
+++ src/pages/AddEdit.jsx
@@ -1,25 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate,useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import './AddEdit.css';
 import dbRef from '../Firebase';
 import { toast } from 'react-toastify';
 
 function AddEdit() {
-    const initialState = {
-        bizName: '',
-        address: '',
-        email: '',
-        website: '',
-        contactPersonName: '',
-        phoneNo: '',
-      };
-    
+  const initialState = {
+    bizName: '',
+    address: '',
+    email: '',
+    website: '',
+    contactPersonName: '',
+    phoneNo: '',
+  };
+
   const navigate = useNavigate();
-  const [data,setData]=useState({})
+  const [data, setData] = useState({});
   const [state, setState] = useState(initialState);
-  const {id}=useParams()
+  const { id } = useParams();
+
   useEffect(() => {
-    dbRef.child("biz-contacts").on("value", (snapshot) => {
+    dbRef.child('biz-contacts').on('value', (snapshot) => {
       if (snapshot.val() !== null) {
         setData({ ...snapshot.val() });
       } else {
@@ -28,23 +29,19 @@ function AddEdit() {
     });
     return () => {
       setData({});
-    }
+    };
   }, [id]);
-  useEffect(()=>{
-    if(id){
-        setState({...data[id]})
-    } else{
-        setState({...initialState})
+
+  useEffect(() => {
+    if (id) {
+      setState({ ...data[id] });
+    } else {
+      setState({ ...initialState });
     }
-    return ()=>{
-        setState({...initialState})
-    }
-
-  },[id,data])
-
-
-
-  
+    return () => {
+      setState({ ...initialState });
+    };
+  }, [id, data]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -61,28 +58,26 @@ function AddEdit() {
     if (!bizName || !address || !email || !website || !contactPersonName || !phoneNo) {
       toast.error('Please provide a value in each input field');
     } else {
-        if(!id){
-            dbRef.child('biz-contacts').push(state, (err) => {
-                if (err) {
-                  toast.error(err);
-                } else {
-                  toast.success('New Business-Details  Added Successfully');
-                }
-              });
+      if (!id) {
+        dbRef.child('biz-contacts').push(state, (err) => {
+          if (err) {
+            toast.error(err);
+          } else {
+            toast.success('New Business-Details Added Successfully');
+          }
+        });
+      } else {
+        dbRef.child(`biz-contacts/${id}`).set(state, (err) => {
+          if (err) {
+            toast.error(err);
+          } else {
+            toast.success('Business-Details Updated Successfully');
+          }
+        });
+      }
 
-        } else{
-            dbRef.child(`biz-contacts/${id}`).set(state, (err) => {
-                if (err) {
-                  toast.error(err);
-                } else {
-                  toast.success('Business-Details Updated Successfully');
-                }
-              });
-        }
-        
       setTimeout(() => navigate('/'), 2000);
     }
-    // Save the data in state to your database or perform other actions here
   };
 
   return (
@@ -98,22 +93,22 @@ function AddEdit() {
       >
         <div className="form-group">
           <label htmlFor="bizName">Business Name:</label>
-          <input type="text" id="bizName" name="bizName" value={state.bizName || ""} onChange={handleChange} />
+          <input type="text" id="bizName" name="bizName" value={state.bizName || ''} onChange={handleChange} />
         </div>
 
         <div className="form-group">
           <label htmlFor="address">Address:</label>
-          <input type="text" id="address" name="address" value={state.address || ""} onChange={handleChange} />
+          <input type="text" id="address" name="address" value={state.address || ''} onChange={handleChange} />
         </div>
 
         <div className="form-group">
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" value={state.email || ""} onChange={handleChange} />
+          <input type="email" id="email" name="email" value={state.email || ''} onChange={handleChange} />
         </div>
 
         <div className="form-group">
           <label htmlFor="website">Website:</label>
-          <input type="text" id="website" name="website" value={state.website || ""} onChange={handleChange} />
+          <input type="text" id="website" name="website" value={state.website || ''} onChange={handleChange} />
         </div>
 
         <div className="form-group">
@@ -122,17 +117,17 @@ function AddEdit() {
             type="text"
             id="contactPersonName"
             name="contactPersonName"
-            value={state.contactPersonName || ""}
+            value={state.contactPersonName || ''}
             onChange={handleChange}
           />
         </div>
 
         <div className="form-group">
           <label htmlFor="phoneNo">Phone Number:</label>
-          <input type="number" id="phoneNo" name="phoneNo" value={state.phoneNo || ""} onChange={handleChange} />
+          <input type="number" id="phoneNo" name="phoneNo" value={state.phoneNo || ''} onChange={handleChange} />
         </div>
 
-        <input type="submit" value={id? "Update" :"Save"}/>
+        <input type="submit" value={id ? 'Update' : 'Save'} />
       </form>
     </div>
   );
